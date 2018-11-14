97cc1e17b01aa6df5a1da124960d7cbefae05a17
David Tucker <davidt2003@msn.com>
2018-11-14 16:55:17 -0500

    Added comments

--- app/Controllers/ExampleController.php
+++ app/Controllers/ExampleController.php
@@ -5,25 +5,36 @@ class ExampleController extends ApiController
     public function initializeRoutes()
     {
         $this->get("/", "index");
-        $this->post("/login", "createToken");
+        $this->post("/login", "login");
     }
 
     public function index()
     {
+        // If token is enabled, this route will not render unless the client
+        // provides a valid token for the corresponding resource identifier.
         return $this->json(['userId' => $this->resourceIdentifier]);
     }
 
-    public function createToken()
+    public function login()
     {
-        $userId = $this->login();
+        $userId = $this->authenticate();
         if ($userId < 1) {
             return $this->error(["Login failed!"]);
         }
+
+        // Only need to apply the generic resource identifier to automatically
+        // generate a token.
         $this->resourceIdentifier = $userId;
         return $this->success();
     }
 
-    private function login(): int
+    /**
+     * Quick example of authentication, should include database calls for
+     * proper user management.
+     *
+     * @return int
+     */
+    private function authenticate(): int
     {
         $username = $this->request->getParameter('username');
         $password = $this->request->getParameter('password');
