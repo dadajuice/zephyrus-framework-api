51d1513bf61496cd21d8b503d5ce9d2de457c3f3
David Tucker <davidt2003@msn.com>
2018-11-15 09:24:34 -0500

    Removed security controller

--- app/Controllers/ApiController.php
+++ app/Controllers/ApiController.php
@@ -2,9 +2,10 @@
 
 use Models\Token;
 use Zephyrus\Application\Configuration;
+use Zephyrus\Exceptions\IntrusionDetectionException;
 use Zephyrus\Network\Response;
 
-abstract class ApiController extends SecurityController
+abstract class ApiController extends \Zephyrus\Security\Controller
 {
     /**
      * @var string
@@ -40,7 +41,6 @@ abstract class ApiController extends SecurityController
      *   static API KEY.
      *
      * @return Response | bool
-     * @throws \Zephyrus\Exceptions\IntrusionDetectionException
      * @throws \Zephyrus\Exceptions\InvalidCsrfException
      * @throws \Zephyrus\Exceptions\UnauthorizedAccessException
      */
@@ -69,7 +69,28 @@ abstract class ApiController extends SecurityController
                 $this->resourceIdentifier = $token->getResourceIdentifier();
             }
         }
-        parent::before();
+
+        // May throw an UnauthorizedAccessException, InvalidCsrfException or
+        // IntrusionDetectionException. Its possible to catch the exception directly
+        // here or in the error handling file.
+        try {
+            parent::before();
+        } catch (IntrusionDetectionException $exception) {
+            /**
+             * Defines what to do when an attack attempt (mainly XSS and SQL injection) is
+             * detected in the application. The impact value represents the severity of the
+             * attempt. The code below only logs the attempt in the security.log when impact
+             * is equal or higher than 10. Do nothing more to limit false positive effect on
+             * legit users. IntrusionDetection class is a wrapper of the expose library.
+             *
+             * @see https://github.com/enygma/expose
+             */
+            $data = $exception->getIntrusionData();
+            if ($data['impact'] >= 10) {
+                // Do something (logs, ...)
+                // return $this->abortForbidden();
+            }
+        }
         return true;
     }
 
